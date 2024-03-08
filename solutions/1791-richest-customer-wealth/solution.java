ed51ffd32aa6a876cd82448e0d11ef5f03bd0d7e
Naveen <dnaveenvarma06@gmail.com>
2024-03-08 17:56:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (100.00%)

---
+++ solutions/1791-richest-customer-wealth/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i=0, sum=0; i<accounts.length; i++) {
+            for(int j=0; j<accounts[0].length; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(max, sum);
+            sum = 0;
+        }
+        return max;
+    }
+}
