e4490dfd733570633af03fe38c2cf94783402cb7
Naveen <dnaveenvarma06@gmail.com>
2024-03-14 17:40:24 +0000

    [LeetCode Sync] Runtime - 1 ms (99.97%), Memory - 47.2 MB (99.97%)

---
+++ solutions/0966-binary-subarrays-with-sum/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return solve(nums, goal) - solve(nums, goal-1);
+    }
+    static int solve(int nums[], int goal) {
+        int i, j = 0, sum = 0, res = 0;
+        for(i=0; i<nums.length; i++) {
+            sum += nums[i];
+            while(sum > goal && j <= i) {
+                sum -= nums[j];
+                j++;
+            } 
+            if(sum <= goal) {
+                res += i - j + 1;
+            }
+        }
+        return res;
+    }
+}
