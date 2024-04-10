46987aa1c202cabe33cb1648ea5a6ad0ffdb2043
Naveen <dnaveenvarma06@gmail.com>
2024-04-10 15:00:09 +0000

    [LeetCode Sync] Runtime - 4 ms (100.00%), Memory - 42.6 MB (100.00%)

---
+++ solutions/0987-reveal-cards-in-increasing-order/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n = deck.length;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<n; i++) {
+            q.add(i);
+        }
+        int res[] = new int[n];
+        for(int i=0; i<n; i++) {
+            res[q.poll()] = deck[i];
+            q.offer(q.poll());
+        }
+        System.gc();
+        return res;
+    }
+}
