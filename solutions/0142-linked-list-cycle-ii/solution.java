74516430765623acfc8696c639b171c11b8c99f0
Naveen <dnaveenvarma06@gmail.com>
2024-05-12 07:58:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.2 MB (100.00%)

---
+++ solutions/0142-linked-list-cycle-ii/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null && fast != slow) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while(fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
