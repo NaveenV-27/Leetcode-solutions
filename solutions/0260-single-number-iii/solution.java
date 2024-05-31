f08c372b4236d371ffca9986f8c3ae95b23de638
Naveen <dnaveenvarma06@gmail.com>
2024-05-31 16:34:45 +0000

    [LeetCode Sync] Runtime - 296 ms (5.03%), Memory - 45.7 MB (99.91%)

--- solutions/0260-single-number-iii/solution.java
+++ solutions/0260-single-number-iii/solution.java
@@ -1,28 +1,17 @@
 class Solution {
-    int sol[] = new int[2];
-    void addSol(int si, int[] nums, int i) {
-        if(si >= nums.length-1) {
-            if(sol[1] == 0) {
-                sol[1] = nums[nums.length-1];
-            }
-            return;
-        }
-        if(nums[si] == nums[si+1]) {
-            addSol(si+2, nums, i);
-        } else {
-            sol[i] = nums[si];
-            if(i == 1) {
-                return;
-            }
-            addSol(si+1, nums, 1);
-        }
-    }
     public int[] singleNumber(int[] nums) {
-        if(nums.length <= 2) {
-            return nums;
+        
+        ArrayList<Integer> l = new ArrayList<>();
+        for(int i=0; i<nums.length; i++) {
+            if(l.contains(nums[i]))  {
+                l.remove(l.indexOf(nums[i]));
+            } else {
+                l.add(nums[i]);
+            }
         }
-        Arrays.sort(nums);
-        addSol(0, nums, 0);
+        int sol[] = new int[2];
+        sol[0] = l.get(0);
+        sol[1] = l.get(1);
         return sol;
     }
 }
