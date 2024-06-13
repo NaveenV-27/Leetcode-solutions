58ecaadb58caa1c73ed84ab76898c45c54a9113a
Naveen <dnaveenvarma06@gmail.com>
2024-06-13 12:08:17 +0000

    [LeetCode Sync] Runtime - 2 ms (95.40%), Memory - 43.8 MB (100.00%)

---
+++ solutions/2148-minimum-number-of-moves-to-seat-everyone/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int count = 0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int n = seats.length;
+        for(int i=0; i<n; i++) {
+            count += Math.abs(students[i] - seats[i]);
+        }
+        return count;
+    }
+}
