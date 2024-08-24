5f04f0469756f432f30de70e550557e1ec65ad71
Naveen <dnaveenvarma06@gmail.com>
2024-08-24 19:20:06 +0000

    [LeetCode Sync] Runtime - 1 ms (96.91%), Memory - 45.2 MB (99.96%)

---
+++ solutions/0203-remove-linked-list-elements/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return null;
+
+        ListNode curr = head;
+        while(curr != null) {
+            if(curr.next != null && curr.next.val == val) curr.next = curr.next.next;
+            else curr = curr.next;
+        }
+        if(head != null && head.val == val) head = head.next;
+        return head;
+    }
+}
