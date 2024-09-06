d24e61d4af2bd4a9fd190e939ece672fa4963a0b
Naveen <dnaveenvarma06@gmail.com>
2024-09-06 05:10:08 +0000

    [LeetCode Sync] Runtime - 21 ms (86.61%), Memory - 65.8 MB (100.00%)

---
+++ solutions/3501-delete-nodes-from-linked-list-present-in-array/solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> toBeDel = new HashSet<>();
+        for(var num : nums)toBeDel.add(num);
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode curr = dummyHead, prev = dummyHead;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            if(toBeDel.contains(curr.val)){
+                prev.next = nextNode;
+            }else{
+                prev = curr;
+            }
+            curr = nextNode;
+        }
+        return dummyHead.next;
+    }
+}
+
+// class Solution {
+//     public ListNode modifiedList(int[] nums, ListNode head) {
+//         List<Integer> l = new ArrayList<>();
+//         for(int i:nums) {
+//             l.add(i);
+//         }
+//         ListNode dummy = new ListNode();
+//         dummy.next = head;
+//         ListNode curr = dummy;
+//         ListNode prev = dummy;
+//         while(curr != null) {
+//             ListNode nextNode = curr.next;
+//             if(l.contains(curr.val)) prev.next = nextNode;
+//             else prev = curr;
+//             curr = nextNode;
+//         }
+//         return dummy.next;
+//     }
+// }
