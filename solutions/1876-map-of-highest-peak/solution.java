d7d1b19ab2f9fa0de662b5a3cfd601933a8df54f
Naveen <dnaveenvarma06@gmail.com>
2025-01-22 06:26:30 +0000

    [LeetCode Sync] Runtime - 50 ms (24.49%), Memory - 167.3 MB (100.00%)

---
+++ solutions/1876-map-of-highest-peak/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length;
+        int n = isWater[0].length;
+        int height[][] = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(isWater[i][j] == 1) {
+                    q.add(new int[]{i,j});
+                } else {
+                    height[i][j] = -1;
+                }
+            }
+        }
+        int dirs[][] ={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while(!q.isEmpty()) {
+            int arr[] = q.poll();
+            int r = arr[0];
+            int c = arr[1];
+            for(int dir[] : dirs) {
+                int nr = r+dir[0];
+                int nc = c+dir[1];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && height[nr][nc]==-1) {
+                    height[nr][nc] = height[r][c] + 1;
+                    q.add(new int[]{nr, nc});
+                }
+            }
+        }
+        return height;
+    }
+}
