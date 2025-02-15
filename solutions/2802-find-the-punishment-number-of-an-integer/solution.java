3f87e1fb3eb7aeb113860035b6f50feb7d2b98e3
Naveen <dnaveenvarma06@gmail.com>
2025-02-15 12:49:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (100.00%)

---
+++ solutions/2802-find-the-punishment-number-of-an-integer/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int arr[] = {1, 9, 10, 36, 45, 55, 82, 91, 99, 100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
+        int sum = 0;
+        for(int i=0; i<arr.length && arr[i] <= n; i++) {
+            sum += arr[i]*arr[i];
+        }
+        return sum;
+    }
+}
