d059bf99e89674e8d3c1fe271b634585e61fb386
Naveen <dnaveenvarma06@gmail.com>
2025-05-15 18:19:34 +0000

    [LeetCode Sync] Runtime - 1 ms (96.35%), Memory - 45 MB (100.00%)

---
+++ solutions/3143-longest-unequal-adjacent-groups-subsequence-i/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        int n = groups.length;
+        List<String> l = new ArrayList<>();
+        l.add(words[0]);
+        for(int i=1; i<n; i++) {
+            if(groups[i] != groups[i-1]) {
+                l.add(words[i]);
+            }
+        }
+        return l;
+    }
+}
