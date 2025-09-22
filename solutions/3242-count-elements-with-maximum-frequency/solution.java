c8cbe5a14873969a10c7dacbca0a9816dbb180dd
Naveen <dnaveenvarma06@gmail.com>
2025-09-22 18:05:46 +0000

    [LeetCode Sync] Runtime - 1 ms (99.60%), Memory - 42.2 MB (99.98%)

--- solutions/3242-count-elements-with-maximum-frequency/solution.java
+++ solutions/3242-count-elements-with-maximum-frequency/solution.java
@@ -1,23 +1,17 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
+        int map[] = new int[101];
         int max = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] > max) {
-                max = nums[i];
+        for(int i : nums) {
+            map[i]++;
+            max = Math.max(map[i], max);
+        }
+        int res = 0;
+        for(int i : map) {
+            if(i == max) {
+                res += i;
             }
         }
-        int a[] = new int[max + 1];
-        max = 0;
-        for(int i=0; i<nums.length; i++) {
-            a[nums[i]]++;
-            max = Math.max(max, a[nums[i]]);
-        }
-        int c = 0;
-        for(int i=0; i<a.length; i++) {
-            if(a[i] == max) {
-                c += a[i];
-            }
-        }
-        return c;
+        return res;
     }
 }
