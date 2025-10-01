2951a726af70935608f6f4acd0b4aba31ec3bc79
Naveen <dnaveenvarma06@gmail.com>
2025-10-01 04:43:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40 MB (100.00%)

--- solutions/1642-water-bottles/solution.java
+++ solutions/1642-water-bottles/solution.java
@@ -1,10 +1,15 @@
 class Solution {
-    public int numWaterBottles(int num, int ex) {
-        int count = num;
-        while(num >= ex) {
-            count += num/ex;
-            num = (num%ex) + (num/ex);
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drinked = numBottles;
+        int emp = numBottles;
+        int rem = emp;
+        while(rem >= numExchange) {
+            int exchanged = rem / numExchange;
+            drinked += exchanged;
+            emp = exchanged + rem % numExchange;
+            rem = emp;
+            // System.out.println(rem);
         }
-        return count;
+        return drinked;
     }
 }
