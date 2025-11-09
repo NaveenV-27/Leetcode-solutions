c1ef721a18dc291d8b7f7bfa8b02283c149b6f57
Naveen <dnaveenvarma06@gmail.com>
2025-11-09 07:28:16 +0000

    [LeetCode Sync] Runtime - 3 ms (12.45%), Memory - 42.2 MB (47.78%)

--- solutions/2288-count-operations-to-obtain-zero/solution.java
+++ solutions/2288-count-operations-to-obtain-zero/solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        int c = 0;
+        int count = 0;
         while(num1 > 0 && num2 > 0) {
             if(num1 >= num2) {
                 num1 -= num2;
             } else {
                 num2 -= num1;
             }
-            c++;
+            count++;
+            if(num1 == 0 || num2 == 0) {
+                return count;
+            }
         }
-        return c;
+        return count;
     }
 }
