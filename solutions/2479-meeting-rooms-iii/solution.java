874a9957d936b1a19b2f0dfd90bdd9d82c6f91f0
Naveen <dnaveenvarma06@gmail.com>
2025-12-27 17:20:03 +0000

    [LeetCode Sync] Runtime - 52 ms (96.61%), Memory - 125.7 MB (100.00%)

--- solutions/2479-meeting-rooms-iii/solution.java
+++ solutions/2479-meeting-rooms-iii/solution.java
@@ -1,42 +1,52 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        int[] count = new int[n];       // Count of meetings per room
-        long[] busy = new long[n];      // When each room becomes free
-
         Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
 
-        for (int[] meeting : meetings) {
-            int start = meeting[0], end = meeting[1];
+        int[] count = new int[n];
+        long[] timer = new long[n];
+
+        int itr = 0;
+
+        while (itr < meetings.length) {
+            int[] curr = meetings[itr];
+            int start = curr[0];
+            int end = curr[1];
+            long dur = end - start;
+
+            int room = -1;
             long earliest = Long.MAX_VALUE;
-            int roomIndex = -1;
-            boolean assigned = false;
+            int earliestRoom = -1;
 
             for (int i = 0; i < n; i++) {
-                if (busy[i] < earliest) {
-                    earliest = busy[i];
-                    roomIndex = i;
+                if (timer[i] < earliest) {
+                    earliest = timer[i];
+                    earliestRoom = i;
                 }
-                if (busy[i] <= start) {
-                    busy[i] = end;
-                    count[i]++;
-                    assigned = true;
+                if (timer[i] <= start) {
+                    room = i;
                     break;
                 }
             }
 
-            if (!assigned) {
-                busy[roomIndex] += (end - start);
-                count[roomIndex]++;
+            if (room != -1) {
+                timer[room] = end;
+                count[room]++;
+            } else {
+                timer[earliestRoom] += dur;
+                count[earliestRoom]++;
             }
+
+            itr++;
         }
 
-        int max = 0, res = 0;
+        int max = 0, idx = 0;
         for (int i = 0; i < n; i++) {
             if (count[i] > max) {
                 max = count[i];
-                res = i;
+                idx = i;
             }
         }
-        return res;
+
+        return idx;
     }
 }
