cc66a4f1e118770b8ffe99f2719c17e132d00581
Naveen <dnaveenvarma06@gmail.com>
2026-01-25 09:59:28 +0000

    [LeetCode Sync] Runtime - 3 ms (88.01%), Memory - 52.4 MB (45.39%)

--- solutions/0200-number-of-islands/solution.java
+++ solutions/0200-number-of-islands/solution.java
@@ -1,24 +1,37 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int islandCount = 0;
+        int count = 0;
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
                 if(grid[i][j] == '1') {
-                    islandCount++;
+                    count++;
                     dfs(grid, i, j);
                 }
             }
         }
-        return islandCount;
+        return count;
     }
-    static void dfs(char grid[][], int i, int j) {
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] != '1') {
+
+    private void dfs(char[][] grid, int row, int col) {
+        if(row<0 || row>=grid.length || col < 0 || col >= grid[0].length || grid[row][col] != '1') {
             return;
         }
-        grid[i][j] = '0';
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j+1);
+        grid[row][col] = '0';
+        dfs(grid, row+1, col);
+        dfs(grid, row-1, col);
+        dfs(grid, row, col+1);
+        dfs(grid, row, col-1);
     }
+
+    // static {
+    //     // Runtime.getRuntime().addShutdownHook(new Thread(()->{
+    //     //     try(java.io.FileWriter fw= new java.io.FileWriter("display_runtime.txt")){
+    //     //         fw.write("0");
+    //     //     }catch(Exception e){
+                
+    //     //     }
+    //     // }));
+    //     Runtime.getRuntime().gc();
+
+    // }
 }
