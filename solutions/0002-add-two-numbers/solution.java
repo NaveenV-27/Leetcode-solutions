ef5802343de05c94ecaaddd1b788cb68e4609a31
Naveen <dnaveenvarma06@gmail.com>
2026-01-30 09:34:54 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 46.9 MB (6.53%)

---
+++ solutions/0002-add-two-numbers/solution.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode();
+        ListNode curr = newHead;
+        int carry = 0;
+        while(l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
+            if(sum >= 10) {
+                sum %= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while(l1 != null) {
+            int sum = carry + l1.val;
+            if(sum >= 10) {
+                sum %= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+            l1 = l1.next;
+        }
+
+        while(l2 != null) {
+            int sum = carry + l2.val;
+            if(sum >= 10) {
+                sum %= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+            l2 = l2.next;
+        }
+
+        if(carry == 1) {
+            curr.next = new ListNode(carry);
+        }
+
+        return newHead.next;
+
+    }
+}
