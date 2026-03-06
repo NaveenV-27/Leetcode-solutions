16b8e923e9e9c72196fd3a5c7e3cf8f44a632daa
Naveen <dnaveenvarma06@gmail.com>
2026-03-06 05:36:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (17.48%)

---
+++ solutions/1910-check-if-binary-string-has-at-most-one-segment-of-ones/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        int n = s.length();
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if(s.charAt(i) == '1') {
+                count++;
+                while(i<n && s.charAt(i) == '1') {
+                    i++;
+                }
+            }
+        }
+        if(count > 1) return false;
+        return true;
+    }
+}
