e9d08cbdcf5afe680d7ee6f481b7cd63a5b9e250
Naveen <dnaveenvarma06@gmail.com>
2026-05-19 10:12:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 80.2 MB (80.90%)

--- solutions/2634-minimum-common-value/solution.java
+++ solutions/2634-minimum-common-value/solution.java
@@ -1,8 +1,15 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        for(int i=0; i<nums1.length; i++) {
-            if(Arrays.binarySearch(nums2, nums1[i]) >= 0) {
-                return nums1[i];
+        int i=0, j=0;
+        int m = nums1.length;
+        int n = nums2.length;
+        if(nums1[m-1] < nums2[0] || nums2[n-1] < nums1[0]) return -1;
+        while(i < m && j < n) {
+            if(nums1[i] == nums2[j]) return nums1[i];
+            if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         return -1;
