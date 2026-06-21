0faf2b69dc4ac2badd8dd69f69e6fb9502bbcbed
Naveen <dnaveenvarma06@gmail.com>
2026-06-21 09:17:16 +0000

    [LeetCode Sync] Runtime - 37 ms (56.81%), Memory - 83.9 MB (42.50%)

---
+++ solutions/1961-maximum-ice-cream-bars/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count = 0;
+        for(int i : costs) {
+            if(i <= coins) {
+                count++;
+                coins -= i;
+            }
+        }
+        return count;
+    }
+}
