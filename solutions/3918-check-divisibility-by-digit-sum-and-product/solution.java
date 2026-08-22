413290c8aead6ff541206062da87c5eebf4db512
Naveen <dnaveenvarma06@gmail.com>
2026-08-22 20:38:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.6 MB (6.66%)

---
+++ solutions/3918-check-divisibility-by-digit-sum-and-product/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum = 0, prod = 1;
+        int temp = n;
+        while(temp > 0) {
+            int dig = temp % 10;
+            sum += dig;
+            prod *= dig;
+            temp /= 10;
+        }
+        return n % (sum + prod) == 0;
+    }
+}
